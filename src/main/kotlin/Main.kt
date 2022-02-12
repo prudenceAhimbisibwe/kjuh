d706b1b21eb984fda2ef0d57998094dd38dcdda8
PudenceAhimbisibwe <prudeahimbisibwe@gmail.com>
2022-02-13 00:02:00 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    var name  = "prudence ahimbisibwe"
+    println(name)
+    var phonenumber = "0784017930"
+    println(phonenumber)
+    var age = "34"
+    println(age)
+    var weight = "60"
+    println(weight)
+    val isugandan: Boolean = true
+    println(isugandan) // output true
+    val isnotugandan: Boolean = false
+    println(isnotugandan) // output false
+
+}
